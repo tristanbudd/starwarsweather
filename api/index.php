943ea07bd84fc38f6c2b6385bbdc1e315dcbe9e3
Tristan <tristanbudd@outlook.com>
2025-10-16 20:16:12 +0100

    Fix vendor autoload.

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'vendor/autoload.php';
+require_once($_SERVER["DOCUMENT_ROOT"] . "/vendor/autoload.php");
 
 function get_document_path($path_type="", $component=false): string
 {
